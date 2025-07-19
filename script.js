e31dbef5665898ca10400c815852ead677691dbc
Urvashiipawar <122180221+Urvashiipawar@users.noreply.github.com>
2025-07-19 19:08:31 +0200

    Replace with new files

---
+++ script.js
@@ -0,0 +1,16 @@
+const nextButton = document.querySelector('.next-btn');
+const video = document.querySelector('.main-video');
+
+const movieList =['videos/mainvideo.mp4', 'videos/mainvideo1.mp4', 'videos/mainvideo2.mp4', 'videos/mainvideo3.mp4',];
+
+let index = 0;
+nextButton.addEventListener('click', function(){
+
+    index+= 1
+    video.src = movieList[index];
+
+    if(index === 3){
+        index= -1;
+    }
+
+})
